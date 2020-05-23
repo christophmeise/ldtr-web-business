7035722a91e00a4b6eb14895e1ab1321eff34b45
Christoph Meise <christoph.meise@gmx.de>
2020-05-23 13:14:32 +0200

    Header Overlay improve CSS

--- src/components/header-overlay/header-overlay.tsx
+++ src/components/header-overlay/header-overlay.tsx
@@ -11,20 +11,22 @@ const HeaderOverlay = ({ sources, color, inverted, content }) => {
                 backgroundColor: color,
             }}
         >
-            <BackgroundImage
-                Tag="section"
-                className="header-overlay-image header-overlay-center-cropped"
-                fluid={sources}
-            >
-                <Container className="header-overlay-container">
-                    <Grid className="header-overlay-container-desktop responsive-desktop-container">
-                        <GridColumn width={8}>{content}</GridColumn>
-                    </Grid>
-                    <Grid className="header-overlay-container-mobile responsive-mobile-container">
-                        <GridColumn>{content}</GridColumn>
-                    </Grid>
-                </Container>
-            </BackgroundImage>
+            <div className="header-overlay-image-wrapper">
+                <BackgroundImage
+                    Tag="section"
+                    className="header-overlay-image header-overlay-center-cropped"
+                    fluid={sources}
+                >
+                    <Container className="header-overlay-container">
+                        <Grid className="header-overlay-container-desktop responsive-desktop-container">
+                            <GridColumn width={8}>{content}</GridColumn>
+                        </Grid>
+                        <Grid className="header-overlay-container-mobile responsive-mobile-container">
+                            <GridColumn>{content}</GridColumn>
+                        </Grid>
+                    </Container>
+                </BackgroundImage>
+            </div>
         </div>
     );
 };

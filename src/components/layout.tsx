c049c6d357728c630a13e6376452a114cc0f40ff
Christoph Meise <christoph.meise@gmx.de>
2020-06-02 15:57:13 +0200

    Cookie popup, data protection page, footer

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,10 @@
+import { Link } from 'gatsby';
 import React from 'react';
+import { cssTransition, toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import 'semantic-ui-less/semantic.less';
 import '../components/i18n/i18n';
+import { getPathWithLocale } from './navigateWithLocale';
 import PageFooter from './page-footer';
 import ResponsiveContainer from './responsive-container/responsive-container';
 
@@ -12,15 +16,41 @@ interface Props {
     headerOverlay?: any;
 }
 
-const Layout = ({ children, invertedHeader, t }: Props) => {
-    return (
-        <ResponsiveContainer invertedHeader={invertedHeader ? true : false} t={t}>
-            <div className="flex-container">
-                <main className="main-container">{children}</main>
-                <PageFooter />
-            </div>
-        </ResponsiveContainer>
-    );
-};
+class Layout extends React.Component<Props> {
+    constructor(props) {
+        super(props);
+    }
+
+    notify = () =>
+        toast(
+            <p>
+                To make this page work, we log user data. By using our services, you agree to our
+                <Link to={getPathWithLocale('/dataprotection')}> Privacy Policy</Link>, including our cookie policy.
+            </p>,
+        );
+
+    componentDidMount() {
+        this.notify();
+    }
+
+    render() {
+        const { children, invertedHeader, t } = this.props;
+        const Zoom = cssTransition({
+            enter: 'Toastify__bounce-enter',
+            exit: 'Toastify__bounce-exit',
+            appendPosition: true,
+        });
+
+        return (
+            <ResponsiveContainer invertedHeader={invertedHeader ? true : false} t={t}>
+                <div className="flex-container">
+                    <ToastContainer position="bottom-center" autoClose={false} closeOnClick transition={Zoom} />
+                    <main className="main-container">{children}</main>
+                    <PageFooter t={t} />
+                </div>
+            </ResponsiveContainer>
+        );
+    }
+}
 
 export default Layout;
--- src/components/page-footer.tsx
+++ src/components/page-footer.tsx
@@ -1,40 +1,105 @@
+import { Link } from 'gatsby';
 import React from 'react';
-import { Container, Grid, Header, List, Segment } from 'semantic-ui-react';
+import { Button, Container, Grid, Header, Icon, List, Segment } from 'semantic-ui-react';
+import Logo from './logo';
+import { getPathWithLocale } from './navigateWithLocale';
 
-const PageFooter = () => {
+const PageFooter = ({ t }) => {
     return (
-        <Segment inverted vertical className="page-footer" style={{ padding: '5em 0em' }}>
-            <Container>
-                <Grid divided inverted stackable>
-                    <Grid.Row>
-                        <Grid.Column width={3}>
-                            <Header inverted as="h4" content="About" />
-                            <List link inverted>
-                                <List.Item as="a">Sitemap</List.Item>
-                                <List.Item as="a">Contact Us</List.Item>
-                                <List.Item as="a">Religious Ceremonies</List.Item>
-                                <List.Item as="a">Gazebo Plans</List.Item>
-                            </List>
-                        </Grid.Column>
-                        <Grid.Column width={3}>
-                            <Header inverted as="h4" content="Services" />
-                            <List link inverted>
-                                <List.Item as="a">Banana Pre-Order</List.Item>
-                                <List.Item as="a">DNA FAQ</List.Item>
-                                <List.Item as="a">How To Access</List.Item>
-                                <List.Item as="a">Favorite X-Men</List.Item>
-                            </List>
-                        </Grid.Column>
-                        <Grid.Column width={7}>
-                            <Header as="h4" inverted>
-                                Footer Header
-                            </Header>
-                            <p>Extra space for a call to action inside the footer that could help re-engage users.</p>
-                        </Grid.Column>
-                    </Grid.Row>
-                </Grid>
-            </Container>
-        </Segment>
+        <footer>
+            <div className="footer-main">
+                <Container>
+                    <Grid stackable>
+                        <Grid.Row>
+                            <Grid.Column width={3}>
+                                <Header className="footer-nav-header" as="h4" content={t('footer-directory')} />
+                                <List link>
+                                    <List.Item>
+                                        <Link to={getPathWithLocale('/')}>{t('aboutme')}</Link>
+                                    </List.Item>
+                                    <List.Item>
+                                        <Link to={getPathWithLocale('/blog')}>{t('blog')}</Link>
+                                    </List.Item>
+                                    <List.Item>
+                                        <Link to={getPathWithLocale('/shop')}>{t('shop')}</Link>
+                                    </List.Item>
+                                    <List.Item>
+                                        <Link to={getPathWithLocale('/contact')}>{t('contact')}</Link>
+                                    </List.Item>
+                                </List>
+                            </Grid.Column>
+                            <Grid.Column width={3}>
+                                <Header className="footer-nav-header" as="h4" content={t('footer-pages')} />
+                                <List link>
+                                    <List.Item>
+                                        <Link to={getPathWithLocale('/book-call')}>{t('book-call')}</Link>
+                                    </List.Item>
+                                    <List.Item>
+                                        <Link to={getPathWithLocale('/faq')}>{t('faq')}</Link>
+                                    </List.Item>
+                                </List>
+                            </Grid.Column>
+                            <Grid.Column width={3}>
+                                <Header className="footer-nav-header" as="h4" content={t('footer-general')} />
+                                <List link>
+                                    <List.Item>
+                                        <Link to={getPathWithLocale('/impressum')}>{t('impressum')}</Link>
+                                    </List.Item>
+                                    <List.Item>
+                                        <Link to={getPathWithLocale('/dataprotection')}>{t('dataprotection')}</Link>
+                                    </List.Item>
+                                </List>
+                            </Grid.Column>
+                            <Grid.Column width={5}>
+                                <Header className="footer-nav-header" as="h4" content={t('footer-services')} />
+                                <Link to={getPathWithLocale('/book-call')}>
+                                    <Button primary={true} inverted={false} size="small">
+                                        {t('book-first-call')}
+                                    </Button>
+                                </Link>
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
+                </Container>
+            </div>
+            <Segment inverted vertical>
+                <Container>
+                    <Grid divided inverted stackable>
+                        <Grid.Row
+                            style={{
+                                display: 'flex',
+                                flexDirection: 'row',
+                                justifyContent: 'space-between',
+                                alignItems: 'center',
+                            }}
+                        >
+                            <Logo inverted={true} />
+                            <a
+                                style={{ color: 'white', margin: '0' }}
+                                href="https://explorechristoph.com"
+                                target="_blank"
+                            >
+                                © 2020, ExploreChristoph. All rights reserved.
+                            </a>
+                            <div>
+                                <a href="https://www.facebook.com" target="_blank">
+                                    <Icon className="hover-animate" size="large" name="facebook" inverted></Icon>
+                                </a>
+                                <a href="https://www.linkedin.com" target="_blank">
+                                    <Icon className="hover-animate" size="large" name="linkedin" inverted></Icon>
+                                </a>
+                                <a href="https://www.instagram.com" target="_blank">
+                                    <Icon className="hover-animate" size="large" name="instagram" inverted></Icon>
+                                </a>
+                                <a href="https://www.youtube.com" target="_blank">
+                                    <Icon className="hover-animate" size="large" name="youtube" inverted></Icon>
+                                </a>
+                            </div>
+                        </Grid.Row>
+                    </Grid>
+                </Container>
+            </Segment>
+        </footer>
     );
 };
 

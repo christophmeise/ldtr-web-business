fb5a8eaeae3f18351f0a019affebde6dd99bc365
Christoph Meise <christoph.meise@gmx.de>
2020-06-08 09:38:39 +0200

    Working on the blog display

--- src/components/blog-post-full-text/blog-post-full-text.tsx
+++ src/components/blog-post-full-text/blog-post-full-text.tsx
@@ -1,30 +1,17 @@
-import Img from 'gatsby-image';
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import './blog-post-full-text.less';
 
 interface Props {
-    content: any;
+content: any;
 }
 
 export default function BlogPostFullText({ content }: Props) {
     return (
         <section className="blog-blog-full-text">
-            <Grid stackable>
-                <Grid.Column width={8}>
-                    <Image className="rounded" wrapped ui={false}>
-                        {content.images != null && (
-                            <Img className="rounded shadow" fluid={content.images[0].childImageSharp.fluid} />
-                        )}
-                    </Image>
-                </Grid.Column>
-                <Grid.Column width={8}>
-                    <h2 className="global-headline">{content.title}</h2>
-                    <article>
-                        <p>{content.content}</p>
-                    </article>
-                </Grid.Column>
-            </Grid>
+            <Container text>
+                <article>{content.content}</article>
+            </Container>
         </section>
     );
 }

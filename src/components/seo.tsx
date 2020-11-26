95d1abe591f1788e3d5fc996b5f63e108a92929c
Shark919 <christoph.meise@gmx.de>
2020-11-26 19:09:15 +0100

    Added Newsletter

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -35,6 +35,18 @@ const SEO = ({ description, meta, title }: Props) => {
                 }}
                 title={title}
                 titleTemplate={`%s | ${site.siteMetadata.title}`}
+                script={[{
+                    type: 'text/javascript',
+                    innerHTML: `
+(function(m,a,i,l,e,r){ m['MailerLiteObject']=e;function f(){
+var c={ a:arguments,q:[]};var r=this.push(c);return "number"!=typeof r?r:f.bind(c.q);}
+f.q=f.q||[];m[e]=m[e]||f.bind(f.q);m[e].q=m[e].q||f.q;r=a.createElement(i);
+var _=a.getElementsByTagName(i)[0];r.async=1;r.src=l+'?v'+(~~(new Date().getTime()/1000000));
+_.parentNode.insertBefore(r,_);})(window, document, 'script', 'https://static.mailerlite.com/js/universal.js', 'ml');
+
+var ml_account = ml('accounts', '2640527', 'j8z7t0x0z3', 'load');
+                    ` 
+                }]}
                 meta={[
                     {
                         name: `description`,

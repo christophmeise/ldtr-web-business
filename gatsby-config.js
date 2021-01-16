a764981b05ea917322aa10ee240277944521035c
Shark919 <christoph.meise@gmx.de>
2021-01-16 16:23:12 +0100

    Google tag stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
         },
     },
     plugins: [
-        {
+/*         {
             resolve: `gatsby-plugin-google-analytics`,
             options: {
                 // The property ID; the tracking code won't be generated without it
@@ -47,6 +47,31 @@ module.exports = {
                 //sampleRate: 5,
                 //siteSpeedSampleRate: 10,
                 //cookieDomain: "example.com",
+            },
+        }, */
+        {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+            // You can add multiple tracking ids and a pageview event will be fired for all of them.
+            trackingIds: [
+                process.env.GATSBY_GOOGLE_TRACKING_ID, // Google Analytics / GA
+                process.env.GATSBY_GOOGLE_ADS_ID, // Google Ads / Adwords / AW
+                // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+            ],
+            // This object gets passed directly to the gtag config command
+            // This config will be shared across all trackingIds
+            gtagConfig: {
+                anonymize_ip: true,
+                cookie_expires: 0,
+            },
+            // This object is used for configuration specific to this plugin
+            pluginConfig: {
+                // Puts tracking script in the head instead of the body
+                head: false,
+                // Setting this parameter is also optional
+                respectDNT: false,
+                // Avoids sending pageview hits from custom paths
+                exclude: [], // "/preview/**", "/do-not-track/me/too/"
             },
         },
         /*  {

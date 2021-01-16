8cf060d108f2ef2237349ddf23bceb87f9ca47d5
Shark919 <christoph.meise@gmx.de>
2021-01-16 16:27:16 +0100

    Fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
         },
     },
     plugins: [
-/*         {
+        /*         {
             resolve: `gatsby-plugin-google-analytics`,
             options: {
                 // The property ID; the tracking code won't be generated without it
@@ -50,39 +50,31 @@ module.exports = {
             },
         }, */
         {
-      resolve: `gatsby-plugin-google-gtag`,
-      options: {
-            // You can add multiple tracking ids and a pageview event will be fired for all of them.
-            trackingIds: [
-                process.env.GATSBY_GOOGLE_TRACKING_ID, // Google Analytics / GA
-                process.env.GATSBY_GOOGLE_ADS_ID, // Google Ads / Adwords / AW
-                // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
-            ],
-            // This object gets passed directly to the gtag config command
-            // This config will be shared across all trackingIds
-            gtagConfig: {
-                anonymize_ip: true,
-                cookie_expires: 0,
-            },
-            // This object is used for configuration specific to this plugin
-            pluginConfig: {
-                // Puts tracking script in the head instead of the body
-                head: false,
-                // Setting this parameter is also optional
-                respectDNT: false,
-                // Avoids sending pageview hits from custom paths
-                exclude: [], // "/preview/**", "/do-not-track/me/too/"
+            resolve: `gatsby-plugin-google-gtag`,
+            options: {
+                // You can add multiple tracking ids and a pageview event will be fired for all of them.
+                trackingIds: [
+                    process.env.GATSBY_GOOGLE_TRACKING_ID, // Google Analytics / GA
+                    process.env.GATSBY_GOOGLE_ADS_ID, // Google Ads / Adwords / AW
+                    // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+                ],
+                // This object gets passed directly to the gtag config command
+                // This config will be shared across all trackingIds
+                gtagConfig: {
+                    anonymize_ip: true,
+                    cookie_expires: 0,
+                },
+                // This object is used for configuration specific to this plugin
+                pluginConfig: {
+                    // Puts tracking script in the head instead of the body
+                    head: false,
+                    // Setting this parameter is also optional
+                    respectDNT: false,
+                    // Avoids sending pageview hits from custom paths
+                    exclude: [], // "/preview/**", "/do-not-track/me/too/"
+                },
             },
         },
-        /*  {
-            resolve: `gatsby-source-google-analytics-reporting-api`,
-            options: {
-                email: process.env.GATSBY_CLIENT_EMAIL,
-                key: process.env.GATSBY_GOOGLE_API_CLIENT_KEY,
-                viewId: `226079807`,
-                startDate: `2020-08-08`,
-            },
-        }, */
         'gatsby-plugin-catch-links',
         'gatsby-plugin-react-helmet',
         'gatsby-plugin-sharp',
